f660cfe76e26868dd30c92e1fae4343be64c3f70
royjonthan20 <royjonathan204@gmail.com>
2024-03-25 21:57:52 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,7 @@
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: ")) 
+    t = int(input("Masukkan tinggi segitiga: ")) 
+    luas = a * t / 2 
+    print("luas segitiga adalah: ", luas) 
+    
+luas_segitiga()  
